de21ea46b87b704e325afa51d09d08c5cccb51c6
Al2yk <sharonovaas@mail.ru>
2025-09-10 19:44:44 +0300

    third commit

--- app/src/main/java/com/example/ser/MainActivity.kt
+++ app/src/main/java/com/example/ser/MainActivity.kt
@@ -29,11 +29,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
